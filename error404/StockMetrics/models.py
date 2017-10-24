f589d47394307bce6bbeccecc7b95ddba2523369
Yanjie Gao <yanjiegao@umass.edu>
2017-10-24 18:08:26 -0400

    Build stock and user models

--- error404/StockMetrics/models.py
+++ error404/StockMetrics/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class Stock(model.Model):
+
+	title = models.Charfield(max_length=20)
+	id = models.Charfield(max_length=20)
+	volume = models.IntegerField()
+	buy_price = models.FloatField()
+	sell_price = models.FloatField()
+
+	def _str_(self):
+		"""
+        String for representing the Model object.
+        """
+		return self.title
+
+class User(model.Model):
+
+	name = models.Charfield(max_length=20)
+	date_of_birth = models.DateField()
+	email = models.Charfield(max_length=20)
+	password = models.Charfield(max_length=20)
+	balance = models.FLoatField()
+
+	def _str_(self):
+		"""
+        String for representing the Model object.
+        """
+		return self.name
+
