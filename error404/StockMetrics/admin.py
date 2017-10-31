0ac4ca706ff65d405fac480ec25948495ab82d70
Yanjie Gao <yanjiegao@umass.edu>
2017-10-31 13:37:23 -0400

    Create stockmetrcs and portfolio models

--- error404/StockMetrics/admin.py
+++ error404/StockMetrics/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Stock, User
+from .models import Stock, User, StockMetrics, Portfolio
 
 admin.site.register(Stock)
 admin.site.register(User)
+admin.site.register(StockMetrics)
+admin.site.register(Portfolio)
