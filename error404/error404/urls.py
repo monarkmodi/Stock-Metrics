4dc2515085032d75c781124ad6aab69b4e44b69b
Wenbo Xie <weboxie@umass.edu>
2017-11-02 00:40:46 -0400

    old

--- error404/error404/urls.py
+++ error404/error404/urls.py
@@ -31,10 +31,10 @@ urlpatterns += [
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
 #Add URL maps to redirect the base URL to our application
 from django.views.generic import RedirectView
 urlpatterns += [
     url(r'^$', RedirectView.as_view(url='/StockMetrics/', permanent=True)),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
