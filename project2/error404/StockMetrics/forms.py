35972f6a717ced12296df7edce24709854bb1dbc
Yanjie Gao <yanjiegao@umass.edu>
2017-11-16 19:09:28 -0500

    Finished rendering the login interface

---
+++ project2/error404/StockMetrics/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label='username',widget=forms.TextInput(attrs={'placeholder':'Username'}))
+    password = forms.CharField(label='password',widget=forms.PasswordInput(attrs={'placeholder':'Password'}))
